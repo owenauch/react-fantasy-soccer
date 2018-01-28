93a80a3557f92d9227bec1c0676697f35f84f916
Owen Auch <owenauch@wustl.edu>
2018-01-28 23:10:02 +0000

    changes

--- src/Lineups.js
+++ src/Lineups.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react'
 
 import Fetcher from './Fetcher'
+import WeekSelector from './WeekSelector.js'
 
 export default class Lineups extends Component {
   constructor (props) {
     super(props)
     this.state = {
       matchweeks: [],
-      matchweek: null
+      matchweek: null,
+      rosters: []
     }
   }
 
   addFetchedDataToState = (field, data) => {
     this.setState({[field]: data})
+  }
+
+  // update state when select changes
+  handleSelectChange = (event) => {
+    this.setState({matchweek: event.target.value})
   }
 
   render () {
@@ -22,6 +29,11 @@ export default class Lineups extends Component {
           addFetchedDataToState={this.addFetchedDataToState}
           url='http://127.0.0.1:8000/api/matchweeks/'
           field='matchweeks'
+        />
+        <WeekSelector
+          matchweeks={this.state.matchweeks}
+          matchweek={this.state.matchweek}
+          handleSelectChange={this.handleSelectChange}
         />
       </div>
     )
